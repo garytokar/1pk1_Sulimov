443357172ca3646eba1e01245a15cf1e39644961
Олег Сулимов <olegsulimov035@gmail.com>
2025-01-26 19:34:01 +0500

    сделано задание 2,4

---
+++ Task_02_04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+
+namespace Task_02_04
+{
+    internal class Program
+    {
+        //Пользователь вводит свою дату рождения построчно
+        //(сначала год, потом месяц и в конце дату)
+        //произведите расчет является ли пользователь совершеннолетним 
+        //на текущую дату и выведите соответствующее сообщение об этом
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год рождения:");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц рождения:");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите день рождения:");
+            int day = int.Parse(Console.ReadLine());
+
+            DateTime today = DateTime.Now;
+            int delta_day = today.Day - day;
+            int delta_month = today.Month - month;
+            if (delta_day < 0) delta_month--;
+            int delta_year = today.Year - year;
+            if (delta_month < 0) delta_year--;
+            Console.WriteLine(delta_year >= 18 ? "Пользователь является совершеннолетним" : "Пользователь не является совершеннолетним");
+        }
+    }
+}
