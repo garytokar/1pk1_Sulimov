7a9d85131edafc7f3d5f538753f30f00cf962af4
Олег Сулимов <olegsulimov035@gmail.com>
2025-01-26 23:04:09 +0500

    задание 6

---
+++ Task_03_06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// Написать программу, которая выводит таблицу значений функции: 𝑦=|𝑥|для -4≤x≤4, с шагом h = 0,5
+namespace Task_03_06
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            double startX = -4.0;
+            double endX = 4.0;
+            double step = 0.5;
+
+            Console.WriteLine("Таблица значений функции y = |x|:");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("   x\t\t   y");
+            Console.WriteLine("---------------------------------------------------");
+
+            for (double x = startX; x <= endX; x += step)
+            {
+                double y = Math.Abs(x);
+                Console.WriteLine($"{x:F1}\t\t{y:F1}");
+            }
+
+            Console.WriteLine("---------------------------------------------------");
+        }
+    }
+}
