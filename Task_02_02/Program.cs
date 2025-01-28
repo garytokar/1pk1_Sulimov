7b84feef0ea7932deed7bdd565778888055aa716
Олег Сулимов <olegsulimov035@gmail.com>
2025-01-28 20:57:03 +0500

    сделано задание 2,4

--- Task_02_02/Program.cs
+++ Task_02_02/Program.cs
@@ -1,18 +1,16 @@
 ﻿namespace Task_02_02
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+        internal class Program
         {
-            int a = 8;
-            int b = 14;
-            double c = Math.PI / 4;
+            static void Main(string[] args)
+            {
+                int a = 8, b = 14;
+                double c = Math.PI / 4;
+                double result = Math.Pow(b + Math.Pow(a - 1, 1.0 / 3.0), 1.0 / 4.0) /
+                                (Math.Abs(a - b) * (Math.Pow(Math.Sin(c), 2) + Math.Tan(c)));
 
-            double step1 = Math.Pow(b + Math.Pow(a - 1, 1.0 / 3.0), 1.0 / 4.0);
-            double step2 = Math.Abs(a - b) * (Math.Pow(Math.Sin(c), 2) + Math.Tan(c));
-            double step3 = step1 / step2;
-
-            Console.WriteLine(step3);
+                Console.WriteLine(result);
+            }
         }
     }
 }
